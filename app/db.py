64e912a0c5dcd1cc2df1557786b52001c7343339
Roger Rojas <roger.rojas.effio@gmail.com>
2020-06-13 01:23:58 -0500

    configurando extensiones

---
+++ app/db.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class BaseModelMixin(db.Model):
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.query.get(id)
