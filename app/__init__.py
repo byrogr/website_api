2ca0ef04d742e58c1c06e10f0a2498d70a4da4e2
Roger Rojas <roger.rojas.effio@gmail.com>
2020-06-14 00:20:52 -0500

    configuracion para levantar la api

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Api
+
+from app.db import db
+from app.api.resources import api_v1
+from app.ext import ma, migrate
+
+
+def create_app(settings_module):
+    app = Flask(__name__)
+    app.config.from_object(settings_module)
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+
+    Api(app, catch_all_404s=True)
+
+    app.url_map.strict_slashes = False
+    app.register_blueprint(api_v1)
+
+    return app
