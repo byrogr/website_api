4bbb5b2b87ed1ac0b9d64bf1a9c17ad74fd20b3e
Roger Rojas <roger.rojas.effio@gmail.com>
2020-07-13 01:12:10 -0500

    change name contact to prospect

--- app/api/resources.py
+++ app/api/resources.py
@@ -1,35 +1,35 @@
 from flask import request, Blueprint
 from flask_restful import Api, Resource
 
-from .schemas import ContactSchema
-from ..models.contact import Contact
+from .schemas import ProspectSchema
+from ..models.prospect import Prospect
 
 api_v1 = Blueprint('api_v1', __name__)
 
-contact_schema = ContactSchema()
+prospect_schema = ProspectSchema()
 
 api = Api(api_v1)
 
 
-class ContactResource(Resource):
+class ProspectResource(Resource):
     def get(self):
-        contacts = Contact.get_all()
-        result = contact_schema.dump(contacts, many=True)
+        prospects = Prospect.get_all()
+        result = prospect_schema.dump(prospects, many=True)
         return result
 
     def post(self):
         data = request.get_json()
-        cd = contact_schema.load(data)
-        contact = Contact(
+        cd = prospect_schema.load(data)
+        contact = Prospect(
             name=cd.get('name'),
             email=cd.get('email'),
             phone=cd.get('phone'),
             subject=cd.get('subject'),
             message=cd.get('message'))
         contact.save()
-        resp = contact_schema.dump(contact)
+        resp = prospect_schema.dump(contact)
         return resp, 201
 
 
-api.add_resource(ContactResource, '/api/v1/contacts/',
-                 endpoint='contact_resource')
+api.add_resource(ProspectResource, '/api/v1/prospects/',
+                 endpoint='prospect_resource')
--- app/api/schemas.py
+++ app/api/schemas.py
@@ -2,7 +2,7 @@ from marshmallow import fields
 from app.ext import ma
 
 
-class ContactSchema(ma.Schema):
+class ProspectSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.String()
     email = fields.String()
